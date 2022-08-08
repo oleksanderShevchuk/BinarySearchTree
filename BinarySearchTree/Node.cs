ba2c091d796d27e4facbae09c2cb52ab50d7c7ed
oleksandr.shevchuk222@gmail.com <oleksandr.shevchuk222@gmail.com>
2022-08-08 13:24:16 +0300

    Add project files.

---
+++ BinarySearchTree/Node.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace BinarySearchTree
+{
+    public class Node<T> : IComparable
+        where T : IComparable
+    {
+        public T Data { get; private set; }
+        public Node<T> Left { get; private set; }
+        public Node<T> Right { get; private set; }
+        public Node(T data)
+        {
+            Data = data;
+        }
+        public Node(T data, Node<T> left, Node<T> right)
+        {
+            Data=data;
+            Left = left;
+            Right = right;
+        }
+        public void Add(T data)
+        {
+            var node = new Node<T>(data);
+            if (node.Data.CompareTo(Data) == -1)
+            {
+                if (Left == null)
+                {
+                    Left = node;
+                }
+                else
+                {
+                    Left.Add(data);
+                }
+            }
+            else
+            {
+                if (Right == null)
+                {
+                    Right = node;
+                }
+                else
+                {
+                    Right.Add(data);
+                }
+            }
+        }
+        public int CompareTo(object obj)
+        {
+            if (obj is Node<T> item)
+            {
+                return Data.CompareTo(obj);
+            }
+            else
+            {
+                throw new ArgumentException("Не совпадение типов");
+            }
+        }
+        public override string ToString()
+        {
+            return Data.ToString();
+        }
+    }
+}
---
+++ BinarySearchTree/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace BinarySearchTree
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var tree = new Tree<int>();
+            tree.Add(5);
+            tree.Add(3);
+            tree.Add(7);
+            tree.Add(1);
+            tree.Add(2);
+            tree.Add(8);
+            tree.Add(6);
+            tree.Add(9);
+            tree.Add(4);
+
+            foreach (var item in tree.Preorder())
+            {
+                Console.Write(item + ", ");
+            }
+            Console.WriteLine();
+
+            foreach (var item in tree.Postorder())
+            {
+                Console.Write(item + ", ");
+            }
+            Console.WriteLine();
+
+            foreach (var item in tree.Inorder())
+            {
+                Console.Write(item + ", ");
+            }
+        }
+    }
+}
